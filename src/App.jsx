262a78f71c15cdd3f6dfb4121232a84184bbb07f
yras279 <yras279@2gmail.com>
2026-02-20 18:57:09 +0530

    implement welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Navbar from "#components/Navbar.jsx";
+import {Welcome, Navbar} from "#components";
 
 const App = () => {
   return (
     <main>
         <Navbar/>
+        <Welcome/>
     </main>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Navbar from "#components/navbar.jsx";
+import Welcome from "#components/welcome.jsx";
+
+export { Navbar, Welcome };
