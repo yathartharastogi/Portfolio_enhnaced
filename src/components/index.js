6347fed0053669681190df533c29c3bb570d3206
yras279 <yras279@2gmail.com>
2026-02-20 20:26:21 +0530

    implemented dock presentation

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "#components/navbar.jsx";
 import Welcome from "#components/welcome.jsx";
+import Dock from "#components/dock.jsx";
 
-export { Navbar, Welcome };
+export { Navbar, Welcome, Dock };
